7fd476d4a970bc1e5602c4da8243012ddcd083dc
Christabelle <christefani@yahoo.com>
2023-08-19 14:41:07 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Camping</h1>
+      <p class = "text">Camping is a form of outdoor recreation or outdoor education involving overnight stays with a basic temporary shelter such as a tent. Camping can also include a recreational vehicle, sheltered cabins, a permanent tent, a shelter such as a bivy or tarp, or no shelter at all. Typically, participants leave developed areas to spend time outdoors, in pursuit of activities providing them enjoyment or an educational experience. Spending the night away from home distinguishes camping from day-tripping, picnicking, and other outdoor activities.</p>
+      <img src="https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" />
+      <h2>Common Camping Equipment</h2>
+      <p>Below is a list of a few items common among campers</p>
+      <ul class = "list">
+        <li>Tents</li>
+        <li>Sleeping bags</li>
+        <li>First-aid kits</li>
+        <li>Hiking boots</li>
+        <li>Flashlights</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
